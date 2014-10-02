a9dcd994872e8935e72855a28c5ff64a150a393e
Austin Cherry <chillingwithim@gmail.com>
2014-10-01 21:29:37 -0700

    debug logging

--- hub.go
+++ hub.go
@@ -85,6 +85,7 @@ func (h *hub) closeConnections(c *connection) {
 	delete(h.clients, c.name)
 	for _, name := range c.channels {
 		conns := h.channels[name]
+		log.Println("before conns:", conns)
 		for i, conn := range conns {
 			if c == conn {
 				h.channels[name] = append(conns[:i], conns[i+1:]...)
@@ -93,6 +94,7 @@ func (h *hub) closeConnections(c *connection) {
 				conn.send <- Message{Name: c.name, Body: "", ChannelName: name, OpCode: UnBindOpCode}
 			}
 		}
+		log.Println(" after conns:", conns)
 	}
 }
 
@@ -109,6 +111,7 @@ func (h *hub) addConnection(c *connection) {
 			log.Println("peer already bound", c.name)
 		}
 	} else if h.clients[c.name] == nil {
+		log.Println("add client:", c.name)
 		h.clients[c.name] = c
 	}
 }
@@ -123,7 +126,7 @@ func (h *hub) ifConnectionExist(name string) bool {
 
 //bind to a connection to a channel
 func (h *hub) bindChannel(b broadcastWriter) {
-	//log.Printf("Binding: %s to channel: %s", b.conn.name, b.message.ChannelName)
+	log.Printf("Binding: %s to channel: %s", b.conn.name, b.message.ChannelName)
 	//we haven't seen this channel before and need to let the peers know
 	if !b.conn.peer {
 		if h.channels[b.message.ChannelName] == nil {
@@ -141,9 +144,11 @@ func (h *hub) bindChannel(b broadcastWriter) {
 func (h *hub) unbindChannel(b broadcastWriter) {
 	conns := h.channels[b.message.ChannelName]
 	for i, conn := range conns {
+		log.Println("before conns:", conns)
 		if b.conn == conn {
 			h.channels[b.message.ChannelName] = append(conns[:i], conns[i+1:]...)
 			h.processUnbindChannel(b.message.ChannelName)
+			log.Println("after conns:", conns)
 			break
 		}
 	}
@@ -153,7 +158,7 @@ func (h *hub) unbindChannel(b broadcastWriter) {
 func (h *hub) processUnbindChannel(channelName string) {
 	//delete channel if no longer in use
 	if len(h.channels[channelName]) <= len(h.peers) {
-		//log.Println("deleting channel:", channelName)
+		log.Println("deleting channel:", channelName)
 		delete(h.channels, channelName)
 	}
 }
